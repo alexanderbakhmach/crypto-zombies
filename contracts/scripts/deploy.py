d460080f13bcadc7fd2a6a9955fcd2a142ea7c0e
oleksander <alexandrbakhmach@gmail.com>
2022-04-11 15:43:07 +0300

    Implement generic token metadata.

--- contracts/scripts/deploy.py
+++ contracts/scripts/deploy.py
@@ -6,12 +6,10 @@ def main():
     account = accounts.add(config["wallets"]["from_key"])
     zombie_token_name = "Zombie Token"
     zombie_token_symbol = "ZTK"
-    zombie_ipfs_url = "https://gateway.pinata.cloud/ipfs/QmcsDmVCWiLDHKzAzC6No13SeJNyBXPzVPvaug5Q2NFJoX/"
 
     zombie = Zombie.deploy(
     	zombie_token_name, 
     	zombie_token_symbol, 
-    	zombie_ipfs_url, 
     	{"from": account}, 
     	publish_source=True
     )
